93665940cf5bc8c161310b2ac806a053f36bd4c2
Xyncross1111 <anasmk2003@gmail.com>
2024-01-11 01:42:50 +0530

    fix: next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {
-    webpack: (config) => {
-      config.resolve.fallback = { fs: false };
-  
-      return config;
-    },
-  };
+module.exports = nextConfig
