1c4fc80b4abd76b88a9b7374b860b9c7f231c8b1
Xyncross1111 <anasmk2003@gmail.com>
2024-01-14 20:43:33 +0530

    update: readme update: css rules

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-## TimeTrack
+# TimeTrack
 
 TimeTrack is a NEXT.js project that helps with display interactive schedule.
+
+## Contributors
+
+[Anas Khan](https://github.com/Xyncross1111)
+[Aditya Giri](https://github.com/adityagiri3600)
--- pages/index.tsx
+++ pages/index.tsx
@@ -71,7 +71,7 @@ export default function Home() {
     return (
         <>
             <h1 className={"title"}>TimeTrack</h1>
-            <h2 className={"info"}>By <a href="https://www.linkedin.com/in/anasmkhan/" target="_blank">Anas Khan</a>, <a href="https://www.linkedin.com/in/adityagiri3600/" target="_blank">Aditya Giri</a></h2>
+            <h2 className={"info"}><a href="https://github.com/Xyncross1111/timetrack">Repo</a></h2>
             <div className="datetime-batch-container">
                 <Datetime date={date} />
                 <Batch batch={batch} setBatch={setBatch} setClasses={setClasses} weekDay={weekDay} />
